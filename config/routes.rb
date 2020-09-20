ee897a8937997b5b58d80d6abcd476f7f95f2aa7
naren.gaindhar <narendra.gaindhar@protonshub.in>
2020-09-20 17:41:13 +0530

    seed add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
